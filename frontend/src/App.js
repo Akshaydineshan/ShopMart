c8e848a01e203556197ba4ac7c742b719b6f4997
akshaymv <Akshaymv.prm@gmail.com>
2023-01-02 10:25:17 +0530

    starting-frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <a>
+          Shopmart
+        </a>
+      </header>
+      <main>
+        list
+      </main>
+    </div>
+  );
+}
+
+export default App;
